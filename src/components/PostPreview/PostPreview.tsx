9082ddaf492202887d2baf9de583138bc7e25d7b
Logan <lsaunders275@gmail.com>
2021-12-06 12:54:37 -0700

    added some lint rules; fixed link

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -1,5 +1,5 @@
-import { Link } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Link } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Tags } from '../Tags'
 
 interface PreviewProps {
@@ -15,20 +15,19 @@ interface PreviewProps {
   large: boolean
 }
 
-export const PostPreview = ({ img, title, date, excerpt, tags, to, large }: PreviewProps) => {
-  console.log({ tags })
+export const PostPreview = ({
+  img, title, date, excerpt, tags, to, large,
+}: PreviewProps) => {
   const image = getImage(img.src)
   return (
-    <div className={large ? 'grid grid-cols-2 gap-5 md:col-span-12' : 'col-span-1 md:col-span-4'}>
-      <Link to={to}>
-        <GatsbyImage className={`rounded-lg w-full col-span-1 max-h-60 ${large ? 'h-full' : ''}`} image={image} alt={img.alt} />
-      </Link>
+    <Link to={to} className={large ? 'grid grid-cols-2 gap-5 md:col-span-12' : 'col-span-1 md:col-span-4'}>
+      <GatsbyImage className={`rounded-lg w-full col-span-1 max-h-60 h-60 ${large ? 'h-full' : ''}`} image={image} alt={img.alt} />
       <div>
         <Tags tags={tags} />
-        <h1 className='font-bold text-3xl text-ink-darkest'>{title}</h1>
-        <span className='text-sm text-ink-base'>{date}</span>
-        <p className='font-medium text-ink-darkest'>{excerpt}</p>
+        <h1 className="font-bold text-3xl text-ink-darkest">{title}</h1>
+        <span className="text-sm text-ink-base">{date}</span>
+        <p className="font-medium text-ink-darkest">{excerpt}</p>
       </div>
-    </div>
+    </Link>
   )
 }
