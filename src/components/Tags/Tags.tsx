c904bfc509c7e1bbc973dcb54b556c66ea9aa745
Logan <lsaunders275@gmail.com>
2021-12-07 09:42:23 -0700

    make tags wrap

--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -1,19 +1,17 @@
-import React from "react"
+import React from 'react'
 
 const tagMap = {
-  'tech': ['bg-yellow-lightest', 'text-yellow-darkest'],
+  tech: ['bg-yellow-lightest', 'text-yellow-darkest'],
   'getting-started': ['bg-green-lightest', 'text-green-darkest'],
-  'react': ['bg-blue-lightest', 'text-blue-darkest']
+  react: ['bg-blue-lightest', 'text-blue-darkest'],
 }
 
-export const Tags = ({ tags }) => {
-  return (
-    <div className='flex gap-1 my-2'>
-      {React.Children.toArray(
-        tags.map((tag) => (
-          <span className={`px-3 py-1 rounded-full ${tagMap[tag.toLowerCase()].join(' ')}`}>{tag}</span>
-        ))
-      )}
-    </div>
-  )
-}
+export const Tags = ({ tags }) => (
+  <div className="flex flex-wrap gap-1 my-2">
+    {React.Children.toArray(
+      tags.map((tag) => (
+        <span className={`px-3 py-1 rounded-full ${tagMap[tag.toLowerCase()].join(' ')}`}>{tag}</span>
+      )),
+    )}
+  </div>
+)
